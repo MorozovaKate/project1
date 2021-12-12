58a5ddf257f442762c076a17357aa578615f68fd
Kate Morozova <morozovawork1@gmail.com>
2021-12-12 12:03:33 +0700

    first commit 12/12/2021

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+// let p = 3;
+// console.log(p);
+
+// const answer = prompt('Сколько тебе лет?', '18');
+// console.log(+answer + 3);
+// const answers = [];
+
+// answers[0] = prompt('Как тебя зовут?', '');
+// answers[1] = prompt('Сколько тебе лет?', '');
+// answers[2] = prompt('Как твоя фамилия?', '');
+
+// console.log(typeof(answers));
+// const user = 'Ivan';
+// console.log(alert(`Привет ${user}!`))
